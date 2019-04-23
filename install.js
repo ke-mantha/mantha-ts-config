6ecd9457ebe353b533a48c85f3bcd687b4d41e88
Karen <dev.kaskar2008@gmail.com>
2019-04-23 17:32:22 +0300

    Update install script

--- install.js
+++ install.js
@@ -1,3 +1,5 @@
 var fs = require('fs');
 
-fs.copyFileSync('./tsconfig.new.json', process.cwd() + '/src/tsconfig.json')
+fs.copyFileSync('./tsconfig.src.json', process.env.INIT_CWD + '/src/tsconfig.json')
+fs.copyFileSync('./tsconfig.root.json', process.env.INIT_CWD + '/tsconfig.json')
+fs.copyFileSync('./tsconfig.module.json', process.env.INIT_CWD)
